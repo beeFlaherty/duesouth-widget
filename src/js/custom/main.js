ab0343f398229ddd479d290dcc2d1af6a7293124
Bee Flaherty <bee.flaherty@gmail.com>
2015-11-23 11:39:33 +0000

    Effort Function added

--- src/js/custom/main.js
+++ src/js/custom/main.js
@@ -7,8 +7,6 @@
 
 				var timezoneElements = document.querySelectorAll("[data-timezone]");
 
-				console.log(timezoneElements);
-
 				if (timezoneElements.length) {
 					var
 						SECOND = 1000,
@@ -25,8 +23,6 @@
 					for (var i in timezoneElements) {
 						if (timezoneElements.hasOwnProperty(i)) {
 							var timezone = parseInt(timezoneElements[i].getAttribute('data-timezone'));
-							console.log(timezone);
-
 							var currentTime = new Date();
 							var offsetTime = currentTime.getTime();
 							offsetTime += currentTime.getTimezoneOffset() * MINUTE; // Get UTC
@@ -58,37 +54,54 @@
 
 					var update = function(UniqueID) {
 						var currentDate = new Date();
-						var remainingTime = currentDate - startDate - DAY;
+						var duration = currentDate - startDate - DAY;
 
-						if (remainingTime < 0) remainingTime = 0;
+						if (duration < 0) duration = 0;
 
-						var days = parseInt(remainingTime / DAY);
-						remainingTime = remainingTime % DAY;
 
-						var hours = parseInt(remainingTime / HOUR);
-						remainingTime = remainingTime % HOUR;
+						var minutesForEffort = Math.floor((duration / 1000) / 60);
+						document.getElementById(UniqueID).setAttribute('data-alapsed', minutesForEffort);
 
-						var minutes = parseInt(remainingTime / MINUTE);
-						remainingTime = remainingTime % MINUTE;
+						var days = parseInt(duration / DAY);
+						duration = duration % DAY;
 
-						var seconds = parseInt(remainingTime / SECOND);
+						var hours = parseInt(duration / HOUR);
+						duration = duration % HOUR;
+
+						var minutes = parseInt(duration / MINUTE);
+						duration = duration % MINUTE;
+
+						var seconds = parseInt(duration / SECOND);
 
 						document.getElementById(UniqueID).innerHTML = days + ' days ' + hours + ' hours ' + minutes + ' min ' + seconds;
 						//set data here
 					};
+
+
 
 					for (var i in countDownElements) {
 						if (countDownElements.hasOwnProperty(i)) {
 
 							var startDate = new Date(countDownElements[i].getAttribute('data-countdown'));
 							var UniqueID = "timer_" + new Date().getTime().toString();
-							countDownElements[i].id = UniqueID;
+
+							if (!countDownElements[i].id) {
+								countDownElements[i].id = UniqueID;
+							}
+
+							var elementID = countDownElements[i].id;
 
 							if (isNaN(startDate.getTime())) {
 								return false; // Only proceed if we have a current date
 							}
 
-							setInterval(update, SECOND, UniqueID);
+							setInterval(update, SECOND, elementID);
+
+							var effortID = elementID + '-effort';
+
+							if (document.getElementById(effortID)) {
+								setInterval(effort, SECOND, elementID, effortID);
+							}
 						}
 
 					}
@@ -99,9 +112,14 @@
 			weather = function() {
 
 				var weatherElements = document.querySelectorAll("[data-cityId]");
-
+				var onJsonLoad = function(xhr) {
+					if (xhr.readyState == 4 && xhr.status == 200) {
+						var myArr = JSON.parse(xhr.responseText);
+						updateWeather(myArr);
+					}
+				};
 				var updateWeather = function(data) {
-					console.log('boom');
+
 					try {
 						var status = data.weather[0].description;
 						var code = data.weather[0].icon;
@@ -114,53 +132,65 @@
 						document.querySelector("#" + weatherElements[i].id + " > .wind_speed").innerHtml = wind_speed;
 						//$(self).find('.wind_dir').html(wind_dir); 
 					} catch (e) {
-						console.log ('error');
+						console.log('error');
 					}
 				};
-
-				var loadCheck = function() {
-								if (xhr.status === 200 ) {
-									alert('Something went wrong.');
-								} else if (xhr.status !== 200) {
-									alert('Request failed.  Returned status of ' + xhr.status);
-								}
-							};
 
 				if (weatherElements.length) {
 
 					for (var i in weatherElements) {
 						if (weatherElements.hasOwnProperty(i)) {
+							//this may need to be switched to latlong
 							var cityId = weatherElements[i].getAttribute('data-cityId');
-							console.log(cityId);
 							var UniqueID = "weather_" + new Date().getTime().toString();
 							weatherElements[i].id = UniqueID;
 
 							var xhr = new XMLHttpRequest();
-							xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=' + cityId + '&units=metric&APPID=031b030c6e7ef548c603b40c69fe5cf2&');
-							xhr.send(null);
-							xhr.onload =  updateWeather();
+							var url = 'http://api.openweathermap.org/data/2.5/weather?id=' + cityId + '&units=metric&APPID=031b030c6e7ef548c603b40c69fe5cf2&';
+
+
+							xhr.onreadystatechange = onJsonLoad(xhr);
+
+							xhr.open('GET', url, true);
+							xhr.send();
 						}
 					}
 				}
 			},
 
-			calories = function() {
-				if ($('*[data-calories]').length) {
+			effort = function(timeID, effortID) {
 
-				}
+				console.log(timeID + ', ' + effortID);
 
-			},
+				var calorieContainer = document.querySelector("#" + effortID + " > p .calories");
+				var calorieRate = calorieContainer.getAttribute('data-calories') / 1440;
+				var calorieID = "cal_" + new Date().getTime().toString();
+				calorieContainer.id = calorieID;
 
-			steps = function() {
+				var stepsContainer = document.querySelector("#" + effortID + " > p .steps");
 
+				var stepsRate = stepsContainer.getAttribute('data-steps') / 1440;
+				var stepID = "step_" + new Date().getTime().toString();
+				stepsContainer.id = stepID;
+				
+				var timeAlapsed = document.getElementById(timeID).getAttribute('data-alapsed');
+
+				var caloriesBurnt = numberWithCommas(Math.round(calorieRate * timeAlapsed));
+
+				var stepsTaken = numberWithCommas(Math.round(stepsRate * timeAlapsed));
+
+				//set to display
+				document.getElementById(calorieID).innerHTML = caloriesBurnt;
+				document.getElementById(stepID).innerHTML = stepsTaken;
 			};
+
+		function numberWithCommas(x) {
+			return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		}
 
 		timezone();
 		countup();
 		weather();
-		calories();
-		steps();
-
 
 	}());
 
